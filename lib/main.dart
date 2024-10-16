a6fe80737f20a1e5c8139338483fb820942d0a4c
Gabriel Franzoi <gfranzoi.barbosa@gmail.com>
2024-10-16 16:33:23 -0300

    Atualiza rotas e fluxo de dados para adicionar/editar peças

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'screens/home_page.dart'; 
-import 'models/peca.dart';       
+import 'screens/home_page.dart';
+import 'models/peca.dart';
 import 'screens/lista_peca_page.dart';
-import 'screens/formulario_peca_page.dart'; 
+import 'screens/formulario_peca_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,15 +16,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
   List<Peca> pecas = [];
-
-  
-  void adicionarPeca(Peca peca) {
-    setState(() {
-      pecas.add(peca);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,34 +25,15 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(), 
+      home: const HomePage(),
       routes: {
-        '/lista': (context) => ListaPecaPage(pecas: pecas), 
-        '/formulario': (context) => FormularioPecaPage(adicionarPeca: adicionarPeca), 
+        '/lista': (context) => PecaListScreen(), 
+        '/formulario': (context) => FormularioPecaPage(adicionarPeca: (peca) {
+              setState(() {
+                pecas.add(peca);
+              });
+            }),
       },
-    );
-  }
-}
-
-
-class ListaPecaPage extends StatelessWidget {
-  final List<Peca> pecas;
-
-  const ListaPecaPage({Key? key, required this.pecas}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Lista de Peças')),
-      body: ListView.builder(
-        itemCount: pecas.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(pecas[index].nome),
-            subtitle: Text('Preço: ${pecas[index].preco}'),
-          );
-        },
-      ),
     );
   }
 }
