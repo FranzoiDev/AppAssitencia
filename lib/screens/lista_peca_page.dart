4f962b579cd604c70268d92b52a48ecc66f3bd2f
Gabriel Franzoi <gfranzoi.barbosa@gmail.com>
2024-10-16 15:57:54 -0300

    atualização automática da lista após a adição ou edição de uma peça

--- lib/screens/lista_peca_page.dart
+++ lib/screens/lista_peca_page.dart
@@ -3,11 +3,59 @@ import '../models/peca.dart';
 import '../services/peca_api.dart';
 import 'formulario_peca_page.dart';
 
-class PecaListScreen extends StatelessWidget {
-  final PecaApi api = PecaApi();
-  final Function(Peca) adicionarPeca;  
+class PecaListScreen extends StatefulWidget {
+  const PecaListScreen({Key? key}) : super(key: key);
 
-  PecaListScreen({Key? key, required this.adicionarPeca}) : super(key: key);
+  @override
+  _PecaListScreenState createState() => _PecaListScreenState();
+}
+
+class _PecaListScreenState extends State<PecaListScreen> {
+  final PecaApi api = PecaApi();
+  List<Peca> pecas = [];
+
+  @override
+  void initState() {
+    super.initState();
+    carregarPecas();
+  }
+
+  void carregarPecas() async {
+    try {
+      List<Peca> pecasApi = await api.getAll();
+      setState(() {
+        pecas = pecasApi;
+      });
+    } catch (error) {
+      showErrorDialog(error.toString());
+    }
+  }
+
+  void showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Erro"),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("Fechar"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void adicionarPeca(Peca peca) {
+    setState(() {
+      pecas.add(peca);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,27 +65,24 @@ class PecaListScreen extends StatelessWidget {
         actions: [
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {
-              Navigator.push(
+            onPressed: () async {
+              Peca? novaPeca = await Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => FormularioPecaPage(adicionarPeca: adicionarPeca),  
+                  builder: (context) =>
+                      FormularioPecaPage(adicionarPeca: adicionarPeca),
                 ),
               );
+              if (novaPeca != null) {
+                adicionarPeca(novaPeca);
+              }
             },
           ),
         ],
       ),
-      body: FutureBuilder<List<Peca>>(
-        future: api.getAll(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Text('Erro: ${snapshot.error}');
-          } else {
-            final pecas = snapshot.data!;
-            return ListView.builder(
+      body: pecas.isEmpty
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
               itemCount: pecas.length,
               itemBuilder: (context, index) {
                 final peca = pecas[index];
@@ -49,38 +94,50 @@ class PecaListScreen extends StatelessWidget {
                     children: [
                       IconButton(
                         icon: const Icon(Icons.edit),
-                        onPressed: () {
-                          Navigator.push(
+                        onPressed: () async {
+                          Peca? pecaEditada = await Navigator.push(
                             context,
                             MaterialPageRoute(
                               builder: (context) => FormularioPecaPage(
-                                adicionarPeca: adicionarPeca,  
+                                adicionarPeca: (pecaEditada) {
+                                  setState(() {
+                                    pecas[index] = pecaEditada;
+                                  });
+                                },
                                 peca: peca,
                               ),
                             ),
                           );
+                          if (pecaEditada != null) {
+                            setState(() {
+                              pecas[index] = pecaEditada;
+                            });
+                          }
                         },
                       ),
                       IconButton(
                         icon: const Icon(Icons.delete),
-                        onPressed: () {
-                          api.delete(peca.id!).then((_) {
+                        onPressed: () async {
+                          try {
+                            await api.delete(peca.id!);
+                            setState(() {
+                              pecas.removeAt(index);
+                            });
                             ScaffoldMessenger.of(context).showSnackBar(
                               const SnackBar(
                                 content: Text('Peça deletada com sucesso!'),
                               ),
                             );
-                          });
+                          } catch (error) {
+                            showErrorDialog(error.toString());
+                          }
                         },
                       ),
                     ],
                   ),
                 );
               },
-            );
-          }
-        },
-      ),
+            ),
     );
   }
 }
