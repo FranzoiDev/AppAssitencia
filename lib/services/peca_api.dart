846730ab83863f5a00f659a08fcd24e8329491fc
Gabriel Franzoi <gfranzoi.barbosa@gmail.com>
2024-10-23 16:00:39 -0300

    implementação de path_provider

--- lib/services/peca_api.dart
+++ lib/services/peca_api.dart
@@ -4,31 +4,55 @@ import '../models/peca.dart';
 import 'crud_api.dart';
 
 class PecaApi extends CrudApi<Peca> {
-  PecaApi() : super("http://localhost:3000/pecas");
+  late String apiUrl;
+
+  PecaApi() : super("") {
+    _initApiUrl();
+  }
+
+  Future<void> _initApiUrl() async {
+    try {
+      
+      final response = await http.get(Uri.parse('http://localhost:3000/getPort'));
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        final port = data['port'];
+        apiUrl = 'http://localhost:$port/pecas';
+      } else {
+        throw Exception('Erro ao obter a porta');
+      }
+    } catch (e) {
+      throw Exception('Erro ao ler a porta: $e');
+    }
+  }
 
   @override
   Future<List<Peca>> getAll() async {
+    await _initApiUrl();  
     final response = await http.get(Uri.parse(apiUrl));
     if (response.statusCode == 200) {
       List<dynamic> body = jsonDecode(response.body);
       return body.map((dynamic item) => Peca.fromJson(item)).toList();
     } else {
-      throw Exception('Failed to load peças');
+      throw Exception('Falha ao carregar as peças');
     }
   }
 
   @override
   Future<Peca> getById(int id) async {
+    await _initApiUrl();
     final response = await http.get(Uri.parse('$apiUrl/$id'));
     if (response.statusCode == 200) {
       return Peca.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to load peça');
+      throw Exception('Falha ao carregar a peça');
     }
   }
 
   @override
   Future<Peca> create(Peca peca) async {
+    await _initApiUrl();
     final response = await http.post(
       Uri.parse(apiUrl),
       headers: {'Content-Type': 'application/json'},
@@ -37,12 +61,13 @@ class PecaApi extends CrudApi<Peca> {
     if (response.statusCode == 201) {
       return Peca.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to create peça');
+      throw Exception('Falha ao criar a peça');
     }
   }
 
   @override
   Future<Peca> update(int id, Peca peca) async {
+    await _initApiUrl();
     final response = await http.put(
       Uri.parse('$apiUrl/$id'),
       headers: {'Content-Type': 'application/json'},
@@ -51,15 +76,16 @@ class PecaApi extends CrudApi<Peca> {
     if (response.statusCode == 200) {
       return Peca.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to update peça');
+      throw Exception('Falha ao atualizar a peça');
     }
   }
 
   @override
   Future<void> delete(int id) async {
+    await _initApiUrl();
     final response = await http.delete(Uri.parse('$apiUrl/$id'));
     if (response.statusCode != 200) {
-      throw Exception('Failed to delete peça');
+      throw Exception('Falha ao deletar a peça');
     }
   }
 }
